c1e76d79e72c3d83fc6c5ad42a73858ab430fccb
snailw <lth010618@gmail.com>
2025-05-20 00:46:35 +0900

    was 배포용 수정

--- guestbook/src/main/java/com/second/guestbook/GuestbookApplication.java
+++ guestbook/src/main/java/com/second/guestbook/GuestbookApplication.java
@@ -2,9 +2,16 @@ package com.second.guestbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GuestbookApplication {
+public class GuestbookApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GuestbookApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GuestbookApplication.class, args);
